bb4131a5e1cc00d703d5450798bebeb200894a2e
shrutiujlan <69189379+shrutiujlan@users.noreply.github.com>
2021-02-13 11:54:29 +0530

    Shruti's initial commit
    Added files

---
+++ ecommerce/js/index.js
@@ -0,0 +1,7 @@
+// 
+// function visible(a){
+//   document.getElementById(a).style.visibility="visible";
+// }
+// function hide(a){
+//   document.getElementById(a).style.visibility="hidden";
+// }
